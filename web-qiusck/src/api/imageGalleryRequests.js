255466ed77255b0fd4b933934fdb81cb7935df6e
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-14 12:33:19 +0430

    image gallery api request

--- web-qiusck/src/api/imageGalleryRequests.js
+++ web-qiusck/src/api/imageGalleryRequests.js
@@ -3,6 +3,27 @@ import axios from 'axios'
 
 
 export const insertImageGallery=async(data)=>{
+    const ImageForm=new FormData();
+
+    try {
+        const data = await axios({
+            method: 'post',
+            url: 'http://localhost:4848/api/post/insert',
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        })
+        console.log("fucking data",data)
+
+
+
+    }
+    catch (error) {
+        const { response } = error
+        if (response?.data) {
+            return response.data
+        } return { error: error.message || error }
+    }
+}
 
 }
 
