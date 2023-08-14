19813d954f907916ac10f2e2df25ebca30495fff
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-14 10:22:34 +0430

    create login page

---
+++ web-qiusck/src/pages/login/login.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+export const Login=()=>{
+    return (
+        <>
+        <div className="container">
+            <div className="main-login">
+                <div className="avatar-login"></div>
+                <div className="username-input"></div>
+                <div className="password-input"></div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
