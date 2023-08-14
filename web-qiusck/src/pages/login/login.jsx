834b17550c6d292c89e8d443aae3a5af3cea7eeb
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-14 11:13:59 +0430

    try to make login page

--- web-qiusck/src/pages/login/login.jsx
+++ web-qiusck/src/pages/login/login.jsx
@@ -1,8 +1,9 @@
 import "./style.css";
+import shahidBackground from '../../assets/Image/defaultShahid.jpg'
  const Login=()=>{
     return (
         <>
-        <div className="container">
+        <div   style={{backgroundImage:`url${shahidBackground}`,height:'1000px',backgroundSize:'cover'}}>
             <div className="main-login">
                 <div className="avatar-login"></div>
                 <div className="username-input"></div>
