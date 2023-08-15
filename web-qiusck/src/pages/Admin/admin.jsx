3cc636ab0303402674d19102637f81235d908803
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-15 12:51:36 +0430

    Revert "every changes shoul be delete"
    This reverts commit 208c7c18bc886356485248ce7d80abd620676ed3.

--- web-qiusck/src/pages/Admin/admin.jsx
+++ web-qiusck/src/pages/Admin/admin.jsx
@@ -1,14 +1,29 @@
 import './style.css';
 import AdminHeaderIcon from '../.../../../assets/icon/AdminHeaderIcon.svg'
 import uk from '../.../../../assets/icon/uk.svg'
-import Grapic from '../../assets/icon/GrapicSide.svg'
 export const Admin=()=>{
     
 
 
   return(
   <>
- 
+  <div className="admin-header">
+    <div className="header-left">
+        <img src={AdminHeaderIcon}/>
+        <h1  style={{fontFamily:'Diba',display:'inline'}}>حادم</h1>
+    </div>
+    <div className="header-center">
+        <input placeholder='کلمه مورد نظر را جستجو کنید' type="text" className="header-searchInput" />
+    </div>
+    <div className="header-right">
+        <img src={uk} />
+        <img src={uk} />
+        <img src={uk} />
+        <img src={uk} />
+       
+    </div>
+
+  </div>
   
   </>
    
