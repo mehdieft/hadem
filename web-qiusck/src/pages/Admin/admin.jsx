2ad7e5b609a210dd5f9fd1966bc4c99db9f7a6f9
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-15 11:40:37 +0430

    try to create admin page

--- web-qiusck/src/pages/Admin/admin.jsx
+++ web-qiusck/src/pages/Admin/admin.jsx
@@ -3,9 +3,20 @@ export const Admin=()=>{
 
 
   return(
-    <>
-    
-    </>
+  <>
+  <div className="admin-header">
+    <div className="header-left">hadem</div>
+    <div className="header-center">
+        <input type="text" />
+    </div>
+    <div className="header-right">
+       
+    </div>
+
+  </div>
+  
+  </>
+   
   )
     
 }
