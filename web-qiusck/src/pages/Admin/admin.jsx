4c20db397712816d527a9810f8a8a59ae9606e64
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-15 13:11:15 +0430

    working on admin

--- web-qiusck/src/pages/Admin/admin.jsx
+++ web-qiusck/src/pages/Admin/admin.jsx
@@ -1,32 +1,37 @@
-import './style.css';
-import AdminHeaderIcon from '../.../../../assets/icon/AdminHeaderIcon.svg'
-import uk from '../.../../../assets/icon/uk.svg'
-export const Admin=()=>{
-    
-
-
-  return(
-  <>
-  <div className="admin-header">
-    <div className="header-left">
-        <img src={AdminHeaderIcon}/>
-        <h1  style={{fontFamily:'Diba',display:'inline'}}>حادم</h1>
-    </div>
-    <div className="header-center">
-        <input placeholder='کلمه مورد نظر را جستجو کنید' type="text" className="header-searchInput" />
-    </div>
-    <div className="header-right">
-        <img src={uk} />
-        <img src={uk} />
-        <img src={uk} />
-        <img src={uk} />
-       
-    </div>
-
-  </div>
-  
-  </>
-   
-  )
-    
-}
+import "./style.css";
+import AdminHeaderIcon from "../.../../../assets/icon/AdminHeaderIcon.svg";
+import uk from "../.../../../assets/icon/uk.svg";
+export const Admin = () => {
+  return (
+    <>
+      <div className="admin-header">
+        <div className="header-left">
+          <img src={AdminHeaderIcon} />
+          <h1 style={{ fontFamily: "Diba", display: "inline" }}>حادم</h1>
+        </div>
+        <div className="header-center">
+          <input
+            placeholder="کلمه مورد نظر را جستجو کنید"
+            type="text"
+            className="header-searchInput"
+          />
+        </div>
+        <div className="header-right">
+          <img src={uk} />
+          <img src={uk} />
+          <img src={uk} />
+          <img src={uk} />
+        </div>
+      </div>
+      <div className="admin-main">
+        <div className="left-nav">
+          <a href="#">click me</a>
+          <a href="#">click me</a>
+          <a href="#">click me</a>
+          <a href="#">click me</a>
+        </div>
+        <div className="content"></div>
+      </div>
+    </>
+  );
+};
