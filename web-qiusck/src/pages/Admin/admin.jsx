07964d96e58e2a7c732f3eba3879266a0fd8b6f5
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-15 12:01:42 +0430

    try to design admin page

--- web-qiusck/src/pages/Admin/admin.jsx
+++ web-qiusck/src/pages/Admin/admin.jsx
@@ -1,13 +1,18 @@
 import './style.css';
+import AdminHeaderIcon from '../.../../../assets/icon/AdminHeaderIcon.svg'
 export const Admin=()=>{
+    
 
 
   return(
   <>
   <div className="admin-header">
-    <div className="header-left">hadem</div>
+    <div className="header-left">
+        <img src={AdminHeaderIcon}/>
+        <h1  style={{fontFamily:'Diba',display:'inline'}}>حادم</h1>
+    </div>
     <div className="header-center">
-        <input type="text" className="header-searchInput" />
+        <input placeholder='کلمه مورد نظر را جستجو کنید' type="text" className="header-searchInput" />
     </div>
     <div className="header-right">
        
