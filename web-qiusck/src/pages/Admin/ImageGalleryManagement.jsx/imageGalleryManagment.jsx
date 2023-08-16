f44cfc53dfde51b64ef838b403212a0e9004cb84
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-16 15:58:15 +0430

    try to learn more about table

--- web-qiusck/src/pages/Admin/ImageGalleryManagement.jsx/imageGalleryManagment.jsx
+++ web-qiusck/src/pages/Admin/ImageGalleryManagement.jsx/imageGalleryManagment.jsx
@@ -1,9 +1,15 @@
-import './style.css';
+import "./style.css";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { useState } from "react";
 
-export const ImageGalleryManagment=()=>{
-    return (
-        <>
-        <h1>this is image gallery dialog</h1>
-        </>
-    )
-}
+export const ImageGalleryManagment = () => {
+    <>
+    </>
+};
--- web-qiusck/src/pages/Admin/VideoGalleryManagment/videoGalleryManagment.jsx
+++ web-qiusck/src/pages/Admin/VideoGalleryManagment/videoGalleryManagment.jsx
@@ -1,4 +1,4 @@
-export const VideoGalleryDialog=()=>{
+export const VideoGalleryManagment=()=>{
     return (
         <>
         this is video gallery
