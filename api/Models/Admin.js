f4b900960a28d858fb9d6f810e3bac25042eb80b
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-20 16:24:17 +0430

    ok Admin works

--- api/Models/Admin.js
+++ api/Models/Admin.js
@@ -2,8 +2,8 @@ const mongoose =require('mongoose');
 
 const AdminSchema= mongoose.Schema({
      name:{required:true,type:String},
-     password:{required:true,type:String ,min:8,},
-     userName:{required:true,min:5}
+     password:{required:true,type:String ,min:8},
+     userName:{required:true,min:5,type:String}
     })
 
 module.exports=mongoose.model('Admin',AdminSchema)
