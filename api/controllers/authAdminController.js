788e9495d6265c6d487a0b2c6171cd82558321f7
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-20 17:37:45 +0430

    ok login and register wworks but we dont need register

--- api/controllers/authAdminController.js
+++ api/controllers/authAdminController.js
@@ -1,6 +1,14 @@
 const Admin=require('../Models/Admin')
 const byript=require('bcryptjs')
 const jwt=require('jsonwebtoken');
+
+
+ function generateToken(username){
+    return jwt.sign(username,process.env.ACCESS_TOKEN,{expiresIn:"10h"})
+}
+
+
+
 exports.login=async (req, res,next)=>{
  const findedAdmin=  await Admin.findOne({username:req.body.username})
  if(findedAdmin){
@@ -8,6 +16,10 @@ const comparePassword=await byript.compare(req.body.password,findedAdmin.passwor
 if(comparePassword==false)return res.status(500).json({message:"username or password is not correct"})
 else{
     console.log("this is for create token______>")
+    const token= generateToken({username:req.body.username})
+    console.log("this is token")
+    res.json({token,username:findedAdmin.username,name:findedAdmin.name})
+  
 }
 
  }else res.status(500).json({message:"username or password is not correct"})
