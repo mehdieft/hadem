e67e871ae84e7c637b1bb6caf7e6f5b0938c56d4
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-20 17:23:09 +0430

    login works

--- api/controllers/authAdminController.js
+++ api/controllers/authAdminController.js
@@ -2,7 +2,15 @@ const Admin=require('../Models/Admin')
 const byript=require('bcryptjs')
 const jwt=require('jsonwebtoken');
 exports.login=async (req, res,next)=>{
-    console.log("this is the login method lets test",req.body)
+ const findedAdmin=  await Admin.findOne({username:req.body.username})
+ if(findedAdmin){
+const comparePassword=await byript.compare(req.body.password,findedAdmin.password)
+if(comparePassword==false)return res.status(500).json({message:"username or password is not correct"})
+else{
+    console.log("this is for create token______>")
+}
+
+ }else res.status(500).json({message:"username or password is not correct"})
 
 }
 exports.register= async (req,res,next) =>{
@@ -14,7 +22,7 @@ exports.register= async (req,res,next) =>{
         password:hashedPassword
     }) 
     await   admin.save();
-    res.status(201).send({message:"register succesfully done",admin})
+    res.status(201).json({message:"register succesfully done",admin})
 
 }
 exports.testRegister=async(req,res,next)=>{
