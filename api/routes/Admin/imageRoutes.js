3513eafa9d8729346e6710408401b29e0c449caf
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-23 09:21:03 +0430

    try to add file

--- api/routes/Admin/imageRoutes.js
+++ api/routes/Admin/imageRoutes.js
@@ -3,22 +3,18 @@ const router=express.Router();
 const multer = require('multer');
 // const testController=require('../../controllers/test')
 const imageCoontroller=require('../../controllers/imageController')
-
-var path = require('path');
-
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads') // specify the path to save the file
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) // use originalname as the new filename
-  }
-})
-
+    destination: 'publlic/uploads/',
+    filename: function(req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
 var upload = multer({ storage: storage })
-
-
 
 router.post('/insert',upload.single('image'),imageCoontroller.insert);
 
 module.exports=router
+
+
+
+
