8ac6cc9c973c8a36cf920f05b695cd49991e26af
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-26 10:45:40 +0430

    adding shahid controller and shahid rourtes

---
+++ api/controllers/shahidController.js
@@ -0,0 +1,10 @@
+
+exports.insert=async(req,res,next)=>{
+
+}
+exports.update= async (req,res,next)=>{
+
+}
+exports.delete=async(req,res,next)=>{
+    
+}
--- api/routes/Admin/shahidRoutes.js
+++ api/routes/Admin/shahidRoutes.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const router=express.Router()
+const shahidController=require('../../controllers/shahidController')
 
 
 
 
-router.post('/insert',)
+router.post('/insert',shahidController.insert);
+router.post('/update',shahidController.update);
+router.post('/delete',shahidController.delete);
 
 
 
