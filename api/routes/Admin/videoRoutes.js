f16a78cc058aa3ab60672371c9c5c1707383f85a
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-26 10:43:00 +0430

    creating shahid controller

--- api/routes/Admin/videoRoutes.js
+++ api/routes/Admin/videoRoutes.js
@@ -1,6 +1,11 @@
 const express=require('express');
+const videoController=require('../../controllers/videoController')
 
 const router=express.Router()
+
+router.post('/insert',videoController.insert)
+router.post('/update',videoController.update);
+router.post('/delete',videoController.delete)
 
 
 module.exports=router
