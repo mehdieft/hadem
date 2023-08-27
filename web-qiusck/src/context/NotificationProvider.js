5b0436923378d08b82d6af5ec4b2cc649a9ed845
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-27 10:30:15 +0430

    adding notification provider

--- web-qiusck/src/context/NotificationProvider.js
+++ web-qiusck/src/context/NotificationProvider.js
@@ -1,58 +1,40 @@
-import { createContext ,React, useContext, useEffect, useRef, useState} from "react"
-const notificationContext=createContext();
-export default function NotificationProvider({children}){
-    const [notification ,setNotification]=useState({
-        type:'',
-        value:''
-    })
-    
-    const notRef=useRef();
-    useEffect(()=> {
-        // notRef.current?.classList.remove('bottom-14','opacity-0')
-        // notRef.current?.style.bottom="10px"
-        console.log("this is fucked up",notRef.current)
-        // notRef.current?.style.opacity=1
-    },[notification.value]) 
-    let timeoutId;
-    const [bgColor,setBgColor]=useState('bg-red-400')
-    const updateNotification=(type,value)=>{
-        if(timeoutId) return clearTimeout(timeoutId)
-        if(!type || !value)return 
-        switch(type){
-            case 'error':
-                setBgColor('#fc81811')
-             break;
-            case 'warning':
-                setBgColor('#f6ad551');
-                break;
-            case 'success':
-                setBgColor('#68d3911')
-                break;
-             default :setBgColor('#fc81811')       
-        }
-        setNotification({type,value})
-        timeoutId= setTimeout(()=>{
-            setNotification({type:'',value:''})
-        },3000)
-    }
-    return (
-        <>
-       <notificationContext.Provider value={{updateNotification}}>
-            {children}
-       </notificationContext.Provider>
-       {notification.value? <p  style={{ backgroundColor:`${bgColor}`,  borderRadius: '9999px', /* rounded-full */
-  fontWeight: 600,/* font-semibold */
-  fontStyle: 'italic',/* italic */
-  padding: '64px', /* px-16 py-2 */
-  margin: '16px', /* m-4 */
-  color: 'white', /* text-white */
-  bottom: '56px', /* bottom-14 */
-  opacity: 0,/* opacity-0 */
-  position: 'fixed', /* fixed */
-  left: '50% ',/* left-1/2 */
-  transform: 'translateX(-50%)'}} ref={notRef} >{notification.value}</p>:null}
-        </>
+import React from 'react';
+import { Snackbar } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
+import { useState,createContext } from 'react';
 
-    )
+
+const NotificationContext=createContext()
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
-export const useNotification=()=> useContext(notificationContext)
+
+export const NotificationProvider = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [severity, setSeverity] = useState('success');
+
+  const openNotification = (message, severity) => {
+    setMessage(message);
+    setSeverity(severity);
+    setOpen(true);
+  };
+
+  const closeNotification = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <NotificationContext.Provider value={openNotification}>
+      {children}
+      <Snackbar open={open} autoHideDuration={6000} onClose={closeNotification}>
+        <Alert onClose={closeNotification} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </NotificationContext.Provider>
+  );
+};
--- web-qiusck/src/index.js
+++ web-qiusck/src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import {NotificationProvider} from './context/NotificationProvider'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-   
+  <NotificationProvider>
+
       <App />
+  </NotificationProvider>
+   
 
   </BrowserRouter>
 );
