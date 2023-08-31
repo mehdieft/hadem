06783f38f77b2b1d92d7be4d1a5c4b6a6d2a5a6e
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-08-31 08:46:55 +0430

    try to make video gallery works

--- web-qiusck/src/pages/Admin/VideoGalleryManagment/videoGalleryManagment.jsx
+++ web-qiusck/src/pages/Admin/VideoGalleryManagment/videoGalleryManagment.jsx
@@ -17,43 +17,227 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import CancelIcon from "@mui/icons-material/Cancel";
+import AddIcon from "@mui/icons-material/Add";
 import { useEffect } from "react";
 
+export const VideoGalleryManagment = () => {
 
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [openDialog, setOPenDialog] = useState(false);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [tableData, setDataTable] = useState([]);
+    const [error, setErorr] = useState("");
+    const [isInsertDialog, setIsInsertDialog] = useState(false);
+    const [idForDelete, setIdForDelete] = useState(null);
+  const columns = [
+    {
+      id: "NO",
+      label: "No",
+      minWidth: 170,
+      align: "right",
+    },
+    {
+      id: "محل تصویر",
+      label: "محل تصویر",
+      minWidth: 370,
+      align: "right",
+    },
+    {
+      id: "تصویر",
+      label: "تصویر",
+      minWidth: 370,
+      align: "right",
+    },
+    {
+      id: "action",
+      label: "اقدامات",
+      minWidth: 370,
+      align: "right",
+    },
+  ];
+  return (
+    <Paper>
+      <TableContainer sx={{ maxHeight: "600px" }}>
+        <Table stickyHeader aria-label="test for image Gallery">
+          <TableHead>
+            <TableRow>
+              <TableRow>
+                <TableCell align="right" width={300}>
+                  alt
+                </TableCell>
+                <TableCell align="right" width={300}>
+                  video
+                </TableCell>
+                <TableCell align="right" width={300}>
+                  title
+                </TableCell>
+                <TableCell align="right" width={300}>
+                  actions
+                </TableCell>
+                <TableCell align="right" width={300}>
+                  <Tooltip title="اضافه کردن">
+                    <IconButton>
+                      <AddIcon />
+                    </IconButton>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+              {columns.map((index, column) => {
+                <TableCell key={Math.random() * 1000}>
+                  {column.label}
+                </TableCell>;
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item, index) => {
+                const value = item.id;
+                return (
+                  <div key={index * 4}>
+                    <TableRow>
+                      <TableCell align="right" width={300} scope="td">
+                        {" "}
+                        {index + 1 + page * 10}
+                      </TableCell>
+                      <TableCell align="right" width={300}>
+                        <img
+                          src={`http://localhost:4848/static/uploads/${item.url}`}
+                          style={{
+                            borderRadius: "50%",
+                            width: "100px",
+                            height: "100px",
+                          }}
+                          alt=""
+                        />
+                        {/* {item.url + index} */}
+                      </TableCell>
+                      <TableCell align="right" width={300}>
+                        {item.title + index}
+                      </TableCell>
 
+                      <TableCell align="right" width={300}>
+                        <Tooltip title="ویرایش ">
+                          <IconButton
+                            key={Math.random() * 1000}
+                            aria-label="delete"
+                          >
+                            <EditIcon
+                              onClick={() => {
+                                editButtonHandler(item);
+                              }}
+                              sx={{ color: "#d1670c" }}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title="حذف کردن">
+                          <IconButton>
+                            <DeleteIcon
+                              sx={{ color: "red" }}
+                              onClick={() => {
+                                deleteImageItem(item);
+                              }}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                      </TableCell>
+                    </TableRow>
+                  </div>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog
+        sx={{
+          "& .MuiDialog-paper": {
+            width: "80%",
+            maxHeight: 435,
+          },
+        }}
+        open={openDialog}
+      >
+        <DialogTitle sx={{ padding: 0, marginBottom: "12px" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <IconButton onClick={cancelDialog} color="secondary">
+              <CancelIcon />
+            </IconButton>
+            {isInsertDialog == true ? (
+              <span>اضافه کردن عکس جدید</span>
+            ) : (
+              <span>به روز رسانی عکس </span>
+            )}
 
-export const VideoGalleryManagment=()=>{
-
-
-    const columns = [
-        {
-          id: "NO",
-          label: "No",
-          minWidth: 170,
-          align: "right",
-        },
-        {
-          id: "محل تصویر",
-          label: "محل تصویر",
-          minWidth: 370,
-          align: "right",
-        },
-        {
-          id: "تصویر",
-          label: "تصویر",
-          minWidth: 370,
-          align: "right",
-        },
-        {
-          id: "action",
-          label: "اقدامات",
-          minWidth: 370,
-          align: "right",
-        },
-      ];
-    return (
-        <>
-        this is video gallery
-        </>
-    )
-}
+            <div></div>
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <div className="dialog-container">
+              <form>
+                <div>
+                  <TextField
+                    fullWidth
+                    sx={{
+                      width: "350px",
+                      marginTop: "32px",
+                    }}
+                    label="عنوان عکس"
+                    variant="outlined"
+                    value={dataForDialog.title}
+                    onChange={handleTitleChange}
+                  />
+                </div>
+                <div>
+                  <TextField
+                    fullWidth
+                    sx={{
+                      width: "350px",
+                      margin: "32px 0",
+                    }}
+                    label="متن جایگزین"
+                    variant="outlined"
+                    value={dataForDialog.alt}
+                    onChange={handleAltChange}
+                  />
+                </div>
+                <Button variant="outlined" color="secondary" component="label">
+                  Upload Image
+                  <input type="file" hidden onChange={handleImageUpload} />
+                </Button>
+              </form>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={cancelDialog}>بستن</Button>
+          {isInsertDialog == true ? (
+            <Button onClick={saveInsertDialogHandler}>اضافه کردن</Button>
+          ) : (
+            <Button onClick={() => saveDialogUpdateHandle()}>
+              به روز رسانی
+            </Button>
+          )}
+        </DialogActions>
+      </Dialog>
+      {/* // ) : null // } */}
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={tableData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+};
