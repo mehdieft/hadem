2a69819592028576a6d65948dd2ea2bb2756da0e
mehdieft <50948075+mehdieft@users.noreply.github.com>
2023-09-22 13:34:54 +0330

    style not working

--- web-qiusck/src/layouts/Header/header.jsx
+++ web-qiusck/src/layouts/Header/header.jsx
@@ -1,28 +1,57 @@
-import { Link } from 'react-router-dom'
-import videoheader from '../../assets/video/navyHeader.mp4'
-import navy from '../../assets/icon/navy2.png'
-import './style.css'
-export const Header=()=>{
-    return (
-        <>
-        <div className="header-main-wrapper" style={{background:`url(${navy}) 0 0 `}}>
-            <div className="header-links">
-                <Link className="header-link-item"> خانه</Link>
-                <Link className="header-link-item"> خانه</Link>
-                <Link className="header-link-item"> خانه</Link>
-                <Link className="header-link-item"> خانه</Link>
-            </div>
-            <div className="header-text-container">
-                <div className="image-header-section"></div>
-                <div className="heder-text">
-                    <p>نیرویی راهبردی در جهت حفظ تمامیت ارضی آب های نیلگون خلیج فارس</p>
-
-                </div>
-
-            </div>
+import { Link } from "react-router-dom";
+import videoheader from "../../assets/video/navyHeader.mp4";
+import navy from "../../assets/icon/navy2.png";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import logoHeader from "../../assets/icon/logo.svg";
+import "./style.css";
+export const Header = () => {
+  return (
+    <>
+      <div
+        className="header-main-wrapper"
+        style={{ background: `url(${navy}) 0 0 ` }}
+      >
+        <div className="header-links">
+          <Link className="header-link-item"> خانه</Link>
+          <Link className="header-link-item"> خانه</Link>
+          <Link className="header-link-item"> خانه</Link>
+          <Link className="header-link-item"> خانه</Link>
         </div>
-     
-
-        </>
-    )
-}
+        <div className="header-text-container">
+          <div className="image-header-section">
+            <img src={logoHeader} alt="" />
+          </div>
+          <div className="heder-text">
+            <p
+              style={{
+                color: "white",
+                fontFamily: "iranSans",
+                fontSize: "30px",
+              }}
+            >
+              {" "}
+              آرشیوی از افتخارات نیروی دریایی جمهوری اسلامی ایران در دوران دفاع
+              مقدس
+            </p>
+            <span className="header-button">
+              <PlayArrowIcon className="header-icon" />
+              مشاهده بیشتر
+            </span>
+          </div>
+        </div>
+        <div className="header-video-container">
+          <video
+            className="video-header"
+            preload="metadata"
+            loop
+            muted
+            autoPlay
+          >
+            <source type="video/mp4" src={videoheader}></source>
+          </video>
+        </div>
+        <div>dasljkdajklfjksdfsdfsdf</div>
+      </div>
+    </>
+  );
+};
