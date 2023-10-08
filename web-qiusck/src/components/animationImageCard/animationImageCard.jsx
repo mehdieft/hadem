fd07f9c0eb6ef101647ce97c03317aa2dfc5df6f
mehdieft <50948075+mehdieft@users.noreply.github.com>
2023-10-08 12:22:09 +0330

    try to learn animations

--- web-qiusck/src/components/animationImageCard/animationImageCard.jsx
+++ web-qiusck/src/components/animationImageCard/animationImageCard.jsx
@@ -2,14 +2,20 @@ import './style.css';
 import {delay, motion} from 'framer-motion'
 export const AnimationImageCard=({animationImage})=>{
     return (
-        <motion.div initial={{}} 
+        <motion.div initial={{backgroundImage: `url(${animationImage})`,backgroundSize:'cover',display:'flex',justifyContent:'flex-end',flexDirection:'column',height:'71vh'}} 
         animate={{boxShadow: ' 0px 5px 15px transparent'}}
         whileHover={{boxShadow: ' 0px 5px 15px white',scale:1.03,transition: { duration:1 },}}
         transition={{delay:'0.9s',}}
         style={{height:'100%'}}>
-            <motion.div className="animation-card-container" style={{backgroundImage: `url(${animationImage})`}}>
+            <motion.div className="animation-card-container"  >
+                <motion.div>
+                    <span>مشاهده خاطرات</span>
+                </motion.div>
+                <motion.div whileHover={{display:'visible',backgroundColor:'red',opacity:1}} initial={{opacity:0,color:'white'}}>
+                    خاطرات به یاد ماندنی جانبازان معزز نیروی دریایی ارتش جمهوری اسلامی ایران
+                    از دوران دفاع مقدس
+                </motion.div>
 
-             بیسمنتبسیتمبسی
             </motion.div>
 
         
--- web-qiusck/src/layouts/Main/mainLayout.jsx
+++ web-qiusck/src/layouts/Main/mainLayout.jsx
@@ -1,13 +1,14 @@
 import './style.css';
 import {AnimationImageCard} from '../../components/animationImageCard/animationImageCard'
+import firstImage from '../../assets/images/navy-aviation-jobs-careers.jpg'
 
 export const MainLayout=()=>{
     return (
-        <div style={{backgroundColor:'#01202C',paddingTop:'120px'}}>
+        <div style={{backgroundColor:'#01202C',paddingTop:'120px',paddingBottom:'160px'}}>
             <div className="main-image-container">
-            <AnimationImageCard/>
-                <div style={{borderRadius:'5px',backgroundColor:'yellow'}}></div>
-                <div style={{borderRadius:'5px',backgroundColor:'green'}}></div>
+            <AnimationImageCard animationImage={firstImage} />
+                <div style={{height:'120px',borderRadius:'5px',backgroundColor:'yellow'}}></div>
+                <div style={{height:'120px',borderRadius:'5px',backgroundColor:'green'}}></div>
 
             </div>
 
