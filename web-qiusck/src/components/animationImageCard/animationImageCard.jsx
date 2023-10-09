a66b3b30d5eb1a00b74082b6121ba3ce8475c85b
mehdieft <50948075+mehdieft@users.noreply.github.com>
2023-10-09 11:24:10 +0330

    animation works

--- web-qiusck/src/components/animationImageCard/animationImageCard.jsx
+++ web-qiusck/src/components/animationImageCard/animationImageCard.jsx
@@ -1,24 +1,60 @@
-import './style.css';
-import {delay, motion} from 'framer-motion'
-export const AnimationImageCard=({animationImage})=>{
+import "./style.css";
+import { delay, motion } from "framer-motion";
+
+export const AnimationImageCard = ({ animationImage }) => {
+    const containerAnimate = {
+      init: {
+        backgroundImage: `url(${animationImage})`,
+        backgroundSize: "cover",
+        display: "flex",
+        justifyContent: "flex-end",
+        flexDirection: "column",
+        height: "71vh",
+        transition:{delay: "0.9s" }
+      },
+      hover:{ boxShadow: " 0px 5px 15px white",
+      scale: 1.03,
+      transition: { duration: 1 },},
+      animate:{boxShadow: " 0px 5px 15px transparent" }
+    };
+    const titleAnimation={
+        init:{
+            color:'white',
+            fontSize:32,
+            fontWeight:'bold'
+
+        },
+        hover:{color:'#ff9800'}
+    }
+
+    const contextAnimation={
+        init:{opacity: 0, color: "white" },
+        hover:{
+            display: "visible",
+            opacity: 1,
+            padding:'32px 0'
+
+        }
+    }
     return (
-        <motion.div initial={{backgroundImage: `url(${animationImage})`,backgroundSize:'cover',display:'flex',justifyContent:'flex-end',flexDirection:'column',height:'71vh'}} 
-        animate={{boxShadow: ' 0px 5px 15px transparent'}}
-        whileHover={{boxShadow: ' 0px 5px 15px white',scale:1.03,transition: { duration:1 },}}
-        transition={{delay:'0.9s',}}
-        style={{height:'100%'}}>
-            <motion.div className="animation-card-container"  >
-                <motion.div>
-                    <span>مشاهده خاطرات</span>
-                </motion.div>
-                <motion.div whileHover={{display:'visible',backgroundColor:'red',opacity:1}} initial={{opacity:0,color:'white'}}>
-                    خاطرات به یاد ماندنی جانبازان معزز نیروی دریایی ارتش جمهوری اسلامی ایران
-                    از دوران دفاع مقدس
-                </motion.div>
-
-            </motion.div>
-
-        
+    <motion.div
+    variants={containerAnimate}
+      initial="init"
+      animate="animate"
+      whileHover="hover"
+      style={{ height: "100%" }}
+    >
+      <motion.div className="animation-card-container">
+        <motion.div variants={titleAnimation}>
+          <span>مشاهده خاطرات</span>
         </motion.div>
-    )
-}
+        <motion.div
+        variants={contextAnimation}
+        >
+          خاطرات به یاد ماندنی جانبازان معزز نیروی دریایی ارتش جمهوری اسلامی
+          ایران از دوران دفاع مقدس
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
