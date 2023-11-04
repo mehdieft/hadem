67f5070e897d719d99a95b8ec42171f25c5aa1e0
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-11-04 11:32:54 +0330

    adding diary

--- web-qiusck/src/pages/Diary/diary.jsx
+++ web-qiusck/src/pages/Diary/diary.jsx
@@ -3,6 +3,8 @@ import SingleDiaryComponent from  '../../components/SingleDiary/singleDiaryCompo
 import cityImage from "../../assets/images/City-1-1310x500-e1565952871524-1400x499.png";
 import diaryImage from '../../assets/images/diary/samadi.jpg'
 import samadi from '../../assets/Video/houshangSamadi.mp4'
+import khoram1 from '../../assets/images/khoram1.jpg';
+import khoram2 from '../../assets/images/khoram2.jpg'
 
 
 
@@ -10,9 +12,9 @@ const Diary = () => {
   return (
     <>
       <div className="diary-main">
-      <SingleDiaryComponent backgroundImage={cityImage}  titleImage={diaryImage} degree="ناخدا" name="هوشنگ صمدی"  diary="خاطره نجات سرباز رایان"  quete="زمانی که ناقوس مرگ به صدا در آمد جایی برای فرار نیست" videoItem={samadi}  />
+      <SingleDiaryComponent backgroundImage={khoram1}  titleImage={diaryImage} degree="ناخدا" name="هوشنگ صمدی"  diary="خاطره نجات سرباز رایان"  quete="زمانی که ناقوس مرگ به صدا در آمد جایی برای فرار نیست" videoItem={samadi}  />
 
-      <SingleDiaryComponent backgroundImage={cityImage}  titleImage={diaryImage} degree="ناخدا" name="هوشنگ صمدی"  diary="خاطره نجات سرباز رایان"  quete="زمانی که ناقوس مرگ به صدا در آمد جایی برای فرار نیست"   />
+      <SingleDiaryComponent backgroundImage={khoram2}  titleImage={diaryImage} degree="ناخدا" name="هوشنگ صمدی"  diary="خاطره نجات سرباز رایان"  quete="زمانی که ناقوس مرگ به صدا در آمد جایی برای فرار نیست"   />
 
         <SingleDiaryComponent backgroundImage={cityImage}  titleImage={diaryImage} degree="ناخدا" name="هوشنگ صمدی"  diary="خاطره نجات سرباز رایان"  quete="زمانی که ناقوس مرگ به صدا در آمد جایی برای فرار نیست"   />
         
