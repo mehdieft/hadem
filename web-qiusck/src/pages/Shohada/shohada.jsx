1ce90f0dab1bd1457380887ce9bd607ae20cb4f3
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-11-26 10:44:54 +0330

    fv

--- web-qiusck/src/pages/Shohada/shohada.jsx
+++ web-qiusck/src/pages/Shohada/shohada.jsx
@@ -22,23 +22,56 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Modal from "@mui/material/Modal";
 
 // import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 // import DatePicker from 'react-modern-calendar-datepicker';
 // import { Calendar } from "react-modern-calendar-datepicker";
 const VALID_NOTES = [
-  { name: "فروردین", value: "1" },
-  { name: "اردیبهشت", value: "2" },
-  { name: "خرداد", value: "3" },
-  { name: "تیر", value: "4" },
-  { name: "مرداد", value: "5" },
-  { name: "شهریور", value: "6" },
-  { name: "مهر", value: "7" },
-  { name: "آبان", value: "8" },
-  { name: "آذر", value: "9" },
-  { name: "دی", value: "10" },
-  { name: "بهمن", value: "11" },
-  { name: "اسفند", value: "12" },
+  { name: "فروردین", value: 1 },
+  { name: "اردیبهشت", value: 2 },
+  { name: "خرداد", value: 3 },
+  { name: "تیر", value: 4 },
+  { name: "مرداد", value: 5 },
+  { name: "شهریور", value: 6 },
+  { name: "مهر", value: 7 },
+  { name: "آبان", value: 8 },
+  { name: "آذر", value: 9 },
+  { name: "دی", value: 10 },
+  { name: "بهمن", value: 11 },
+  { name: "اسفند", value: 12 },
+];
+const days = [
+  { name: "1", value: 1 },
+  { name: "2", value: 2 },
+  { name: "3", value: 3 },
+  { name: "4", value: 4 },
+  { name: "5", value: 5 },
+  { name: "6", value: 6 },
+  { name: "7", value: 7 },
+  { name: "8", value: 8 },
+  { name: "9", value: 9 },
+  { name: "10", value: 10 },
+  { name: "11", value: 11 },
+  { name: "12", value: 12 },
+  { name: "13", value: 13 },
+  { name: "14", value: 14 },
+  { name: "15", value: 15 },
+  { name: "16", value: 16 },
+  { name: "17", value: 17 },
+  { name: "18", value: 18 },
+  { name: "19", value: 19 },
+  { name: "20", value: 20 },
+  { name: "21", value: 21 },
+  { name: "22", value: 22 },
+  { name: "23", value: 23 },
+  { name: "24", value: 24 },
+  { name: "25", value: 25 },
+  { name: "26", value: 26 },
+  { name: "27", value: 27 },
+  { name: "28", value: 28 },
+  { name: "29", value: 29 },
+  { name: "30", value: 30 },
 ];
 
 const Shohada = () => {
@@ -51,10 +84,20 @@ const Shohada = () => {
   let day = "";
   const [monthName, seMonthName] = useState("");
   const [dayName, setDayName] = useState("");
-  const [note, setNote] = React.useState("");
+  const [monthDie, setMonthDie] = React.useState("");
+  const [dayDie, setDayDie] = useState("");
+  let maheKiri=''
 
-  const handleChange = (event) => {
-    setNote(event.target.value);
+  const handleChangeday = (event) => {
+    setDayDie(event.target.value);
+  };
+  const handleChangeMonth = (evenet) => {
+    setMonthDie(evenet.target.value);
+  };
+  const handleBackdropClick = (event) => {
+    //these fail to keep the modal open
+    event.stopPropagation();
+    return false;
   };
   const Availble = () => {
     // const minimumDate = {
@@ -130,26 +173,14 @@ const Shohada = () => {
     };
     return (
       <>
-        <Tooltip title="جستجو">
-          <motion.IconButton color="white" onClick={handleClickOpen}>
-            <SettingsIcon color="white" style={{ color: "white" }} />
-          </motion.IconButton>
-        </Tooltip>
-        {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button> */}
-        <Dialog disableBackdropClick open={open} width="600">
-          <DialogTitle>Subscribe</DialogTitle>
-          <DialogContent>
-            <div style={{ display: "flex", flexDirection: "row", gap: "32px" }}>
-              <div>
-                <InputLabel id="demo-simple-select-label">ماه شهادت</InputLabel>
+
+{/* <InputLabel id="demo-simple-select-label">ماه شهادت</InputLabel>
                 <Select
                   label="روز شهادت"
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
-                  value={note}
-                  onChange={handleChange}
+                  value={dayDie}
+                  onChange={handleChangeday}
                   MenuProps={{
                     PaperProps: {
                       sx: {
@@ -164,7 +195,9 @@ const Shohada = () => {
                   }}
                 >
                   {VALID_NOTES.map((validNote) => (
-                    <MenuItem value={validNote}>{validNote}</MenuItem>
+                    <MenuItem value={validNote.value}>
+                      {validNote.name}
+                    </MenuItem>
                   ))}
                 </Select>
               </div>
@@ -174,8 +207,8 @@ const Shohada = () => {
                   label="ماه شهادت"
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
-                  value={note}
-                  onChange={handleChange}
+                  value={monthDie}
+                  onChange={handleChangeMonth}
                   MenuProps={{
                     PaperProps: {
                       sx: {
@@ -189,18 +222,50 @@ const Shohada = () => {
                     },
                   }}
                 >
-                  {VALID_NOTES.map((validNote) => (
-                    <MenuItem value={validNote}>{validNote}</MenuItem>
+                  {days.map((validNote) => (
+                    <MenuItem value={validNote.value}>
+                      {validNote.name}
+                    </MenuItem>
                   ))}
-                </Select>
+                </Select> */}
+
+
+
+        <Tooltip title="جستجو">
+          <motion.IconButton color="white" onClick={handleClickOpen}>
+            <SettingsIcon color="white" style={{ color: "white" }} />
+          </motion.IconButton>
+        </Tooltip>
+        {/* <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button> */}
+        <Dialog
+          onBackdropClick={handleBackdropClick}
+          disableEscapeKeyDown
+          open={open}
+          width="100vw"
+          sx={{ "& .MuiDialog-paper": { width: "80%", maxHeight: 435 } }}
+          maxWidth="xs"
+        >
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <form>
+
+            <div style={{ display: "flex", flexDirection: "row", gap: "32px" }}>
+              <div>
+               
               </div>
             </div>
+            </form>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
             <Button onClick={handleClose}>Subscribe</Button>
           </DialogActions>
         </Dialog>
+        
+        
+   
       </>
     );
   };
@@ -209,7 +274,13 @@ const Shohada = () => {
   const fetchAndSetData = async () => {
     const result = await getTodayShahid();
     console.log("ress----->", result);
-    if (result.data.s) seMonthName(result.data.todayDate.month);
+    if (result.data.s){
+      maheKiri=result.data.todayDate.month
+
+      seMonthName(result.data.todayDate.month);
+    } 
+    console.log('_______________>',monthName)
+    seMonthName(result.data.todayDate.month);
     setDayName(result.data.todayDate.day);
     if (result.data !== null) {
       setShahidList(result.data.todayShahid);
