3daf9bc315eec49eaaac7f19d085ee193c7a8b00
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-12-05 09:39:44 +0330

    adding basic footer design

--- web-qiusck/src/layouts/Footer/footer.jsx
+++ web-qiusck/src/layouts/Footer/footer.jsx
@@ -14,6 +14,21 @@ const Footer=()=>{
                 <div className="footer-header-title">
                     ما برای شما شما برای ما
                 </div>
+                <div className="footer-links-inner">
+                    <div className="footer-links-section">
+                        <div className="footer-link-and-button-container">
+                            <div className="footer-button"></div>
+                            <div className="footer-links">
+                                <div><a href=""></a><a href=""></a><a href=""></a></div>
+                                <div><a href=""></a><a href=""></a><a href=""></a></div>
+                               
+                            </div>
+                        </div>
+                        <div className="footer-link-line"></div>
+                        <div className="footer-icons-group"></div>
+                    </div>
+                    <div className="footer-icon"></div>
+                </div>
             </div>
         </div>
 
