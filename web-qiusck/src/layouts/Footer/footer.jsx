51bf79911b302519d2dfd6cb5dfca2985183f831
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-12-05 14:04:00 +0330

    oik

--- web-qiusck/src/layouts/Footer/footer.jsx
+++ web-qiusck/src/layouts/Footer/footer.jsx
@@ -1,5 +1,6 @@
 import './style.css';
-
+import { RadiusButton } from '../../components/RadiusButton/radiusButton';
+import footerImage from '../../assets/images/footer.png'
 
 
 
@@ -9,18 +10,20 @@ import './style.css';
 export  const Footer=()=>{
     return(
         <>
-        <div className="footer-container">
+        <div className="footer-container" style={{background:`url(${footerImage})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
             <div className="footer-content-inner">
                 <div className="footer-header-title">
-                    ما برای شما شما برای ما
+                   fdsfdsfsdfsdfsd ما برای شما شما برای ما
                 </div>
                 <div className="footer-links-inner">
                     <div className="footer-links-section">
                         <div className="footer-link-and-button-container">
-                            <div className="footer-button"></div>
+                            <div className="footer-button">
+                                <RadiusButton title="see more"/>
+                            </div>
                             <div className="footer-links">
-                                <div><a href=""></a><a href=""></a><a href=""></a></div>
-                                <div><a href=""></a><a href=""></a><a href=""></a></div>
+                                <div><a href="">jjaskdghha</a><a href=""></a>kllfdjlkdsjflsjdf<a href="">dfkjlklsdjfklsjdkfljsd</a></div>
+                                <div><a href="">jjaskdghha</a><a href=""></a>kllfdjlkdsjflsjdf<a href="">dfkjlklsdjfklsjdkfljsd</a></div>
                                
                             </div>
                         </div>
