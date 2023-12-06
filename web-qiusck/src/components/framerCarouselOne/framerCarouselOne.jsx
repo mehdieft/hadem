82d564a187de6d5a56cbd899d6bb9b6b9eb76394
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-12-06 09:14:23 +0330

    try to install popcorn

--- web-qiusck/src/components/framerCarouselOne/framerCarouselOne.jsx
+++ web-qiusck/src/components/framerCarouselOne/framerCarouselOne.jsx
@@ -1,8 +1,65 @@
+import './style.css';
+import React, { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "@popmotion/popcorn"
+
+
+
+
+
+
 export const FramerCarouselOne=()=>{
     return (
         <>
+        <main>
+      <div className="slider-container">
+        <div className="slider">
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={imageCount}
+              style={{
+                backgroundImage: `url(${IMAGES[activeImageIndex].imageSrc})`
+              }}
+              custom={direction}
+              variants={sliderVariants}
+              initial="incoming"
+              animate="active"
+              exit="exit"
+              transition={sliderTransition}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(_, dragInfo) => dragEndHandler(dragInfo)}
+              className="image"
+            />
+          </AnimatePresence>
+        </div>
 
-        
+        <div className="buttons">
+          <button onClick={() => swipeToImage(-1)}>PREV</button>
+          <button onClick={() => swipeToImage(1)}>NEXT</button>
+        </div>
+      </div>
+
+      <div className="thumbnails">
+        {IMAGES.map(image => (
+          <div
+            key={image.id}
+            onClick={() => skipToImage(image.id)}
+            className="thumbnail-container"
+          >
+            <img src={image.imageSrc} alt="Musician" />
+            <div
+              className={`active-indicator ${
+                image.id === activeImageIndex ? "active" : null
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
+
+
         </>
         )
 }
