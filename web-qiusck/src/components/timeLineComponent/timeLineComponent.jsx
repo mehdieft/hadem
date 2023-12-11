89b49f3643fdf8b561a57eb2bd97d5ff41eff1a7
mehdi saedi <50948075+mehdieft@users.noreply.github.com>
2023-12-11 12:16:59 +0330

    adding content

--- web-qiusck/src/components/timeLineComponent/timeLineComponent.jsx
+++ web-qiusck/src/components/timeLineComponent/timeLineComponent.jsx
@@ -1,344 +1,118 @@
 import { Chrono } from "react-chrono";
-const TimeLineComponent=()=>{
-    const items = [{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
-        }
-      },{
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg",url: "http://someurl/image.jpg",url: "http://someurl/image.jpg",url: "http://someurl/image.jpg"
-          }
-        }
+import firstImage from "../../assets/images/timeline/1.jpg";
+const TimeLineComponent = () => {
+  const items = [
+    {
+      title: "سی و یکم شهریور ماه 1359",
+      cardSubtitle: "عملیات دفاع 34 روزه از خرمشهر",
+      cardDetailedText: "اولین عملیات دفاع مقدس",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
       },
-     ];
-    return (
-        <div style={{backgroundColor:'rgb(1 32 44)'}}>
-        <Chrono
+    },
+    {
+      title: "چهاردهم مهر ماه سال 1359",
+      cardSubtitle: "عملیات تاخت شبانه در پل نوشهر",
+      cardDetailedText:
+        "این عملیات از سوی گردان یکم تکاوران دریایی برای انهدام متجاوزان و تاخیر در پیشروی آن ها اجرا شد",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
+      },
+    },
+    {
+      title: "بیست و چهارم دی 1359",
+      cardSubtitle: "عملیات آفند و پدافند در محور ماهشهر_آبادان (عملیات شفق)",
+      cardDetailedText:
+        "این عملیات از 1359/10/24 با تحویل مواضع از یگان های ژاندارمری آغاز شد وتا 1360/4/10 با عقب نشینی اجباری دشمن ارز جنوب محور آبادان به ماهشهر خاتمه یافت",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
+      },
+    },
+    {
+      title: "نهم آبان 1359",
+      cardSubtitle:
+        "عملیات اشکان",
+      cardDetailedText:
+        "این عملیات برای انهدام تجهیزات حساس انتقال نفت به نفتکش ها و نیز انهدام نیروهای مستقر یا مدافع رژیم بعثی در ترمینال های نفتی به اجرا در آمد.",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
+      },
+    },
+    {
+      title: "سیزدهم آبان 1359",
+      cardSubtitle:
+        "عملیات شهید صفری",
+      cardDetailedText:
+       "انگیزه اصلی عملیات تصرف و انهدام ترمینال البکر بود و در کنار آن اهداف دیگری نیز دنبال میشد این عملیات از 1359/8/13 تا 1359/8/16 به طول انجامید.",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
+      },
+    },
+    {
+      title: "نهم آبان 1359",
+      cardSubtitle:
+        "عملیات اشکان",
+      cardDetailedText:
+        "این عملیات برای انهدام تجهیزات حساس انتقال نفت به نفتکش ها و نیز انهدام نیروهای مستقر یا مدافع رژیم بعثی در ترمینال های نفتی به اجرا در آمد.",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: firstImage,
+        },
+      },
+    },
+  ];
+  return (
+    <div style={{ backgroundColor: "rgb(1 32 44)" }}>
+      <Chrono
+      className="my-timeline"
+        cardWidth="600"
+        cardHeight="700"
+        mediaHeight="500"
+        style={{ direction: "rtl", fontFamily: "iranSans" }}
         timelinePointShape="diamond"
-           theme={{
-    primary: '#ff7b00',
-    secondary: 'black',
-    cardBgColor: '#f83959b5',
-    titleColor: 'white',
-    titleColorActive: 'white',
-  }} scrollable={{ scrollbar: true }} items={items} />
-
-        </div>
-    )
-}
-export default TimeLineComponent
+        fontSizes={{
+          cardSubtitle: "1.85rem",
+          cardText: "14.8rem",
+          cardTitle: "1rem",
+          title: "1.3rem",
+          cardDetailedText:'1.6rem'
+        }}
+        theme={{
+          primary: "#ff7b00",
+          secondary: "black",
+          cardBgColor: "#468d51",
+          titleColor: "white",
+          titleColorActive: "white",
+        }}
+        scrollable={{ scrollbar: true }}
+        items={items}
+        classNames={{
+          card: 'my-card',
+          cardMedia: 'my-card-media',
+          cardSubTitle: 'my-card-subtitle',
+          cardText: 'my-card-text',
+          cardTitle: 'my-card-title',
+          controls: 'my-controls',
+          title: 'my-title',
+        }}
+      />
+    </div>
+  );
+};
+export default TimeLineComponent;
