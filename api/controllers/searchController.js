c307d1ef85c54e142ad73afcae500717f11873a6
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-08 11:48:49 +0330

    لتن

---
+++ api/controllers/searchController.js
@@ -0,0 +1,30 @@
+const Shahid = require("../Models/Shahid");
+
+exports.nameAndFamilySearch=async(req,res,next)=>{
+  
+    const {name,family}=req.body
+    name.replace('','شهید','');
+    name.split(" ").join("")
+    family.replace('','شهید','');
+    family.split(" ").join("")
+    console.log("this is searchh query",name)
+    
+  const searchedShahid=await Shahid.find({
+     $or: [{ name:name }, { family: family}]
+   
+  }).limit(100)
+  if(searchedShahid){
+    res.json({searchedShahid})
+  }
+  
+}
+exports.city=async(req,res,next)=>{
+    console.log("hello");
+const {city}=req.body;
+const searchedShahid=await Shahid.find({birthdayPlace:city})
+if(searchedShahid){
+    res.json({searchedShahid})
+}
+
+
+}
