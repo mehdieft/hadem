15105853ce711d8bded5c09c97b24a92f753f106
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-16 18:29:40 +0330

    mute and unmute image works

--- api/electron/src/assets/javascript/index.js
+++ api/electron/src/assets/javascript/index.js
@@ -4,19 +4,22 @@ let iconImage=document.getElementsByClassName('image-icon')
 let hiddenImage=document.getElementById('off-icon')
 let showIcon=document.getElementById('on-icon')
 
+
 document.getElementById('sound-button').addEventListener('click',()=>{
     console.log('im working',iconImage)
 
     sound=!sound
     console.log("sound",sound)
     if(sound==false){
-        hiddenImage.classList.remove('hidden')
-        showIcon.classList.add('hidden')
+        hiddenImage.classList.remove('hidden');
+        showIcon.classList.add('hidden');
+        videoOPtions.muted=true
 
     }else{
 
         showIcon.classList.remove('hidden')
         hiddenImage.classList.add('hidden')
+        videoOPtions.muted=false
 
     }
 
