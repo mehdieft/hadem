74d228855a010281b8a1c537c4c8630087ce9149
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-16 18:26:16 +0330

    button works

--- api/electron/src/assets/javascript/index.js
+++ api/electron/src/assets/javascript/index.js
@@ -1,1 +1,23 @@
-let video=document.get
+let sound=false;
+let videoOPtions=document.getElementById('header-video')
+let iconImage=document.getElementsByClassName('image-icon')
+let hiddenImage=document.getElementById('off-icon')
+let showIcon=document.getElementById('on-icon')
+
+document.getElementById('sound-button').addEventListener('click',()=>{
+    console.log('im working',iconImage)
+
+    sound=!sound
+    console.log("sound",sound)
+    if(sound==false){
+        hiddenImage.classList.remove('hidden')
+        showIcon.classList.add('hidden')
+
+    }else{
+
+        showIcon.classList.remove('hidden')
+        hiddenImage.classList.add('hidden')
+
+    }
+
+})
