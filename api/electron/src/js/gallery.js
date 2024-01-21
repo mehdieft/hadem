567ea20e30ecd1f6c5112dda0e6b6acd9eaff702
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-21 08:21:24 +0330

    try to complete image

--- api/electron/src/js/gallery.js
+++ api/electron/src/js/gallery.js
@@ -1,4 +1,14 @@
 let position;
+let images=['./assets/images/gallery/khoramshahr/(1).jpg',
+'./assets/images/gallery/khoramshahr/(2).jpg',
+'./assets/images/gallery/khoramshahr/(3).jpg',
+'./assets/images/gallery/khoramshahr/(4).jpg',
+'./assets/images/gallery/khoramshahr/(6).jpg',
+'./assets/images/gallery/khoramshahr/(7).jpg',
+'./assets/images/gallery/khoramshahr/(8).jpg',
+'./assets/images/gallery/khoramshahr/(12).jpg',
+
+]
 function clickme(item){
     console.log("im here",item)
    let selectedItem= document.getElementById(item);
@@ -15,3 +25,14 @@ function cancelClick(){
 },1000)
    
 }
+function createKhoramShahrImages(){
+    const container=document.getElementById('khoramshahr');
+    const grid=document.getElementsByClassName('grid');
+    for(i=0;i<= images.length;i++){
+        let image=new Image();
+        image.src=images[i];
+        console.log('imagee',image);
+    }
+
+}
+createKhoramShahrImages()
