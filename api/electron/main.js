4c97bafe4384694e8a148abb2b8bc7a0cd1f5d48
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-21 19:45:50 +0330

    fuck

--- api/electron/main.js
+++ api/electron/main.js
@@ -1,17 +1,36 @@
-const {app, BrowserWindow}=require('electron')
+// const {app, BrowserWindow}=require('electron')
+// const TouchscreenWindow = require('electron-touchscreen');
 
 
 
-function createWindow(){
-    const win=new BrowserWindow({
+// function createWindow(){
+//     const win=new BrowserWindow({
 
-        width:600,
-        height:600
-    })
-    win.loadFile('src/index.html')
-}
+//         width:600,
+//         height:600
+//     })
+//     win.loadFile('src/index.html')
+// }
 
-app.whenReady().then(createWindow)
-app.on('window-all-closed',()=>{
-    app.quit()
-})
+// app.whenReady().then(createWindow)
+// app.on('window-all-closed',()=>{
+//     app.quit()
+// })
+const path = require('path');
+const url = require('url');
+const electron = require('electron');
+
+const TouchscreenWindow = require('electron-touchscreen');
+
+let win = null;
+const app = electron.app;
+
+app.on('ready', ()=> {
+  const demo_url = url.format({
+    pathname: path.join(__dirname, 'src/index.html'),
+    protocol: 'file', 
+    slashes: true,
+  });
+
+  win = new TouchscreenWindow({url: demo_url});
+});
