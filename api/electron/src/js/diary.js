b3a89cc3c0efccc1af77a5cde8a63d58bf5bf855
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-01-24 09:39:54 +0330

    github bug fixed

--- api/electron/src/js/diary.js
+++ api/electron/src/js/diary.js
@@ -1,17 +1,34 @@
-function showconsole(){
-    console.log("hello me")
-   let video= document.createElement('video');
-   video.style.height="100%";
-   video.style.width="800px";
-   video.controls=true;
-   let source=document.createElement('source');
-   source.src="./assets/video/amir-sayari.mp4";
-   source.type="video/mp4";
-   video.append(source);
-   let modal=document.getElementsByClassName('modal__bg ')[0];
-   modal.style.display="block";
-   modalContent=document.getElementsByClassName('modal__content')[0]
-   modalContent.append(video);
+function showconsole(item) {
+    console.log("hello me",item)
+    let video = document.createElement('video');
+    video.style.height = "100%";
+    video.style.width = "800px";
+    video.controls = true;
+    let source = document.createElement('source');
+    if (item == "samadi") {
+        source.src = "./assets/video/houshangSamadi.mp4";
+    } else if (item == "sayari") {
+        source.src = "./assets/video/amir-sayari.mp4";
+    } else if (item == "mahbubi") {
+        source.src = "./assets/video/nakhoda-mahboobi.mp4";
+    }
+   
+    source.type = "video/mp4";
+    video.append(source);
+    let modal = document.getElementsByClassName('modal__bg ')[0];
+    modal.style.display = "block";
+    modalContent = document.getElementsByClassName('modal__content')[0]
+    modalContent.append(video);
 
+
+}
+function hiddenDialog(){
+    let dialog=document.getElementsByClassName('modal__bg ')[0]
+    dialog.style.display='none';
+    modalContent = document.getElementsByClassName('modal__content')[0]
+  
+    let video=document.getElementsByTagName('video')[0]
+    console.log("_______________>",video)
+    video.remove()
 
 }
