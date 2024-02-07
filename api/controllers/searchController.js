d2051048ebb6f881649b30fc63b7e1feae2d6a64
mehdieft <50948075+mehdieft@users.noreply.github.com>
2024-02-07 17:54:05 +0330

    search by dieDate works

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -1,52 +1,63 @@
 const Shahid = require("../Models/Shahid");
-const moment=require('moment-jalaali')
+const moment = require('moment-jalaali')
 
-exports.nameAndFamilySearch=async(req,res,next)=>{
-  
-    const {name,family}=req.body
-    name.replace('','شهید','');
-    name.split(" ").join("")
-    family.replace('','شهید','');
-    family.split(" ").join("")
-    console.log("this is searchh query",name,family)
-    const myName=name;
-    const myFamily=family
-    console.log('this is data--------------->',myFamily,myName)
+exports.nameAndFamilySearch = async (req, res, next) => {
 
-    
-  const searchedShahid=await Shahid.find({
-     $or: [{ name:name }, { family: family}]
-   
+  const { name, family } = req.body
+  name.replace('', 'شهید', '');
+  name.split(" ").join("")
+  family.replace('', 'شهید', '');
+  family.split(" ").join("")
+  console.log("this is searchh query", name, family)
+  const myName = name;
+  const myFamily = family
+  console.log('this is data--------------->', myFamily, myName)
+
+
+  const searchedShahid = await Shahid.find({
+    $or: [{ name: name }, { family: family }]
+
   }).limit(100)
-  if(searchedShahid){
-    res.json({searchedShahid})
+  if (searchedShahid) {
+    res.json({ searchedShahid })
   }
-  
+
 }
-exports.city=async(req,res,next)=>{
-    console.log("hello");
-const {city}=req.body;
-const searchedShahid=await Shahid.find({birthdayPlace:city})
-if(searchedShahid){
-    res.json({searchedShahid})
-}
+exports.city = async (req, res, next) => {
+  console.log("hello");
+  const { city } = req.body;
+  const searchedShahid = await Shahid.find({ birthdayPlace: city })
+  if (searchedShahid) {
+    res.json({ searchedShahid })
+  }
 
 
 }
-exports.birthDate=async(req,res,next)=>{
-  const {birthDate}=req.body;
-  const numbericSearch=birthDate.split('/');
-  console.log('this is test for my search',numbericSearch)
-  console.log("this is city____>",birthDate);
-  const searchedShahid=await Shahid.find({
-    $and:[{birthMonth:Number(numbericSearch[1])},{birthDay:Number(numbericSearch[2])}]
+exports.birthDate = async (req, res, next) => {
+  const { birthDate } = req.body;
+  const numbericSearch = birthDate.split('/');
+  console.log('this is test for my search', numbericSearch)
+  console.log("this is city____>", birthDate);
+  const searchedShahid = await Shahid.find({
+    $and: [{ birthMonth: Number(numbericSearch[1]) }, { birthDay: Number(numbericSearch[2]) }]
   })
-  console.log('_______________>shahid',searchedShahid)
-  if(searchedShahid){
-    res.json({searchedShahid});
+  console.log('_______________>shahid', searchedShahid)
+  if (searchedShahid) {
+    res.json({ searchedShahid });
   }
 }
-exports.dieDate=async(req,res,next)=>{
-  const {dieDate}=req.body;
-  console.log("this is die date",dieDate)
+exports.secondAppDieDate = async (req, res, next) => {
+  console.log("im here ",req.body);
+  const { day, month } = req.body;
+  const searchedShahid = await Shahid.find({
+    $and: [{ dieMonth: Number(month) }, { dieDay: Number(day) }]
+  })
+  if (searchedShahid) {
+    res.json({ searchedShahid });
+  }
+
+}
+exports.dieDate = async (req, res, next) => {
+  const { dieDate } = req.body;
+  console.log("this is die date", dieDate)
 }
--- api/routes/Search/searchRoutes.js
+++ api/routes/Search/searchRoutes.js
@@ -3,10 +3,10 @@ const path=require('path')
 const searchController=require('../../controllers/searchController')
 const router=express.Router();
 
-router.post('/searchByName',searchController.nameAndFamilySearch)
+router.post('/searchByName',searchController.nameAndFamilySearch);
 router.post('/searchCity',searchController.city);
 router.post('/searchByBirthDate',searchController.birthDate);
 router.post('/searcchByDieDate',searchController.dieDate);
-
+router.post('/secondSearchByDateDie',searchController.secondAppDieDate);
 
 module.exports=router
